3a2f824c9540553b9bccb3ca38e54429aa631eb4
Oleksii Maslo <aleksej.maslo95@gmail.com>
2026-06-21 15:46:01 +0300

    Add all characters and format

--- draftFormats.js
+++ draftFormats.js
@@ -1,18 +1,29 @@
+const standardSequence = [
+  { type: "ban", p1: true },
+  { type: "ban", p1: false },
+  { type: "ban", p1: true },
+  { type: "ban", p1: false },
+  { type: "ban", p1: true },
+  { type: "ban", p1: false },
+  { type: "pick", p1: true },
+  { type: "pick", p1: false },
+  { type: "pick", p1: false },
+  { type: "pick", p1: true },
+  { type: "pick", p1: true },
+  { type: "pick", p1: false },
+  { type: "pick", p1: false },
+  { type: "pick", p1: true },
+];
+
 export const formats = {
-  standard: [
-    { type: "ban", p1: true },
-    { type: "ban", p1: false },
-    { type: "ban", p1: true },
-    { type: "ban", p1: false },
-    { type: "ban", p1: true },
-    { type: "ban", p1: false },
-    { type: "pick", p1: true },
-    { type: "pick", p1: false },
-    { type: "pick", p1: false },
-    { type: "pick", p1: true },
-    { type: "pick", p1: true },
-    { type: "pick", p1: false },
-    { type: "pick", p1: false },
-    { type: "pick", p1: true },
-  ],
+  test_format: {
+    name: "Test Format",
+    allowedIds: ["all"],
+    sequence: standardSequence,
+  },
+  summer_of_legends_2026: {
+    name: "Summer of Legends 2026",
+    allowedIds: [8, 23, 27, 35],
+    sequence: standardSequence,
+  },
 };
