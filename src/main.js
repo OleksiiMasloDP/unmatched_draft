0bb4dab634c1408ef7737f0efffae4435a2c0791
Oleksii Maslo <aleksej.maslo95@gmail.com>
2026-07-17 12:40:20 +0300

    Fix error with deploy GA

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/style.css";
 import Header from "./components/Header.vue";
-import { VueGtag } from "vue-gtag";
+import * as VueGtag from "vue-gtag";
 
 const app = createApp(App);
-app.use(VueGtag, {
+
+app.use(VueGtag.default || VueGtag, {
   config: { id: "G-6BTH35KNGC" },
 });
 
