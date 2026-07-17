4088677a2de0225ff4803011f96ddb6a5fed212a
Oleksii Maslo <aleksej.maslo95@gmail.com>
2026-07-17 12:42:12 +0300

    Add newest version of module

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/style.css";
 import Header from "./components/Header.vue";
-import * as VueGtag from "vue-gtag";
+import createGtag from "vue-gtag";
 
 const app = createApp(App);
 
-app.use(VueGtag.default || VueGtag, {
-  config: { id: "G-6BTH35KNGC" },
-});
+app.use(
+  createGtag({
+    config: { id: "G-6BTH35KNGC" },
+  }),
+);
 
 app.component("Header", Header);
 
